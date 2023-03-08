2bb1e7b18628e91488af560b8d71d7db26ea849f
luis <luiscarranzaam@unimagdalena.edu.co>
2023-03-08 15:55:20 +0100

    actualizacion

--- lenguajes_formales.py
+++ lenguajes_formales.py
@@ -5,27 +5,39 @@ Created on Mon Mar  6 02:06:16 2023
 @author: SARA CARRANZA
 """ 
 import random
-from potencia import pw
+from potencia import pw,concatenadora
 class Alphabeth():
     simbology=""
     NEUTRAL_ELEMENT="#"
     
     def __init__(self,simbology):
-         self.simbology=simbology    
+         self.simbology=simbology  
+         
+         
     def union (self,alphabet):
-        lista= self.simbology.split(",") + alphabet.simbology.split(",")
-        return lista
+        lista= self.simbology.split(",") 
+        lista2=alphabet.simbology.split(",")
+        return list(set(lista) | set(lista2))
+    
     def intersection(self,alphabet):
-        lista=self.simbology.split(",")+alphabet.simbology.split(",")
-        return lista
+        lista=self.simbology.split(",")
+        lista2=alphabet.simbology.split(",")
+        return list(set(lista) & set(lista2))
+    
     def diference(self, alphabet):
-       lista=self.simbology.split(",")-alphabet.simbology.split(",")
+       lista=self.simbology.split(",")
+       lista2=alphabet.simbology.split(",")
+       return set(lista)-set(lista2)
        return lista
-    def start_loock(string,limit):
+    def start_loock(self,limit):
         lista=[]
-        if limit <=6:  
-           lista.append(pw(string,limit))
-           lista.append("#")
+        if limit <=6:
+            lista.append(self.simbology.split(","))
+            for i in range(limit):
+                   print(i)
+                   if i!=1:
+                       lista.append(pw(self.simbology,i+1))
+            lista.append("#")
         return lista
 class language (Alphabeth) :  
     lista_palabras=[]
@@ -38,3 +50,6 @@ class language (Alphabeth) :
         lista=self.start_loock(self.simbology,5)
         lista=random.SystemRandom().sample(lista, n)
         return lista
+a1=Alphabeth("a,c,m")
+a2=Alphabeth("c,g,m")
+print(a1.start_loock(4))
